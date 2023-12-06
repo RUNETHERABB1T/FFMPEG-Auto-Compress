5e52855ce3dfd9ece36acd22d9270a3dc0621471
Rabbit <48692061+RUNETHERABB1T@users.noreply.github.com>
2023-12-06 06:50:40 +0100

    How did I miss this?
    I made a very simple mistake. Should work now.

--- Mass FFMPEG Compressor (Yes Overwrite) .bat
+++ Mass FFMPEG Compressor (Yes Overwrite) .bat
@@ -4,18 +4,18 @@ mkdir Compressed
 
 ;Image formats
 
-for %%a in ("*.avif")  do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.avif"
-for %%a in ("*.bmp")   do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.bmp"
-for %%a in ("*.bpg")   do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.bpg"
-for %%a in ("*.gif")   do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.gif"
-for %%a in ("*.heif")  do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.heif"
-for %%a in ("*.img")   do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.img"
-for %%a in ("*.jpg")   do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.jpg"
-for %%a in ("*.jpeg")  do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.jpeg"
-for %%a in ("*.png")   do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.png"
-for %%a in ("*.tiff")  do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.tiff"
-for %%a in ("*.webp")  do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.webp"
-for %%a in ("*.dng")   do ffmpeg -y -i "%%a" -map 0 -c:v libsvtav1 -crf 30 "compressed/%%~na.dng"
+for %%a in ("*.avif")  do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.avif"
+for %%a in ("*.bmp")   do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.bmp"
+for %%a in ("*.bpg")   do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.bpg"
+for %%a in ("*.gif")   do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.gif"
+for %%a in ("*.heif")  do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.heif"
+for %%a in ("*.img")   do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.img"
+for %%a in ("*.jpg")   do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.jpg"
+for %%a in ("*.jpeg")  do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.jpeg"
+for %%a in ("*.png")   do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.png"
+for %%a in ("*.tiff")  do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.tiff"
+for %%a in ("*.webp")  do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.webp"
+for %%a in ("*.dng")   do ffmpeg -y -i "%%a" -map 0 -crf 30 "compressed/%%~na.dng"
 
 ;Video Formats
 
